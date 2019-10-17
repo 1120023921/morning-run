c1c757e2da8454dd0b4387bbf0658f61baf3ce10
DoubleH <1120023921@qq.com>
2019-10-17 23:24:46 +0800

    添加视力上报

---
+++ morning-run-biz/src/main/java/cn/doublehh/sport/dao/EyeMapper.java
@@ -0,0 +1,16 @@
+package cn.doublehh.sport.dao;
+
+import cn.doublehh.sport.model.Eye;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ *  Mapper 接口
+ * </p>
+ *
+ * @author 胡昊
+ * @since 2019-10-17
+ */
+public interface EyeMapper extends BaseMapper<Eye> {
+
+}
