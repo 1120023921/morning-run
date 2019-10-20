fe92136b7d20bfcd054af42859e451d0914b4fd3
胡昊 <huhao9277@gmail.com>
2019-10-20 13:29:05 +0800

    用户导入添加提示

--- morning-run-biz/src/main/java/cn/doublehh/system/service/impl/TSUserServiceImpl.java
+++ morning-run-biz/src/main/java/cn/doublehh/system/service/impl/TSUserServiceImpl.java
@@ -6,6 +6,8 @@ import cn.doublehh.system.dao.TSUserMapper;
 import cn.doublehh.system.model.TSUserRole;
 import cn.doublehh.system.service.TSUserRoleService;
 import cn.doublehh.system.service.TSUserService;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import lombok.extern.slf4j.Slf4j;
@@ -75,29 +77,36 @@ public class TSUserServiceImpl extends ServiceImpl<TSUserMapper, TSUser> impleme
                     }
                 }
                 String id = UUID.randomUUID().toString();
-                TSUser tsUser = new TSUser();
-                tsUser.setId(id);
-                tsUser.setUid(row.get(0));
-                tsUser.setName(row.get(1));
-                tsUser.setPassword(row.get(2));
-                tsUser.setSex("男".equals(row.get(3)) ? "1" : "女".equals(row.get(3)) ? "2" : "");
-                tsUser.setTel(row.get(4));
-                tsUser.setMail(row.get(5));
-                tsUser.setQqNumber(row.get(6));
-                tsUser.setWechatNumber(row.get(7));
-                tsUser.setCreateTime(LocalDateTime.now());
-                tsUser.setUpdateTime(LocalDateTime.now());
-                tsUserList.add(tsUser);
-                TSUserRole tsUserRole = new TSUserRole();
-                tsUserRole.setId(UUID.randomUUID().toString());
-                tsUserRole.setUserId(id);
-                tsUserRole.setRoleId(row.get(8));
-                tsUserRoleList.add(tsUserRole);
+                try {
+                    TSUser tsUser = new TSUser();
+                    tsUser.setId(id);
+                    tsUser.setUid(row.get(0));
+                    tsUser.setName(row.get(1));
+                    tsUser.setPassword(row.get(2));
+                    tsUser.setSex("男".equals(row.get(3)) ? "1" : "女".equals(row.get(3)) ? "2" : "");
+                    tsUser.setTel(row.get(4));
+                    tsUser.setMail(row.get(5));
+                    tsUser.setQqNumber(row.get(6));
+                    tsUser.setWechatNumber(row.get(7));
+                    tsUser.setCreateTime(LocalDateTime.now());
+                    tsUser.setUpdateTime(LocalDateTime.now());
+                    tsUserList.add(tsUser);
+                    TSUserRole tsUserRole = new TSUserRole();
+                    tsUserRole.setId(UUID.randomUUID().toString());
+                    tsUserRole.setUserId(id);
+                    tsUserRole.setRoleId(row.get(8));
+                    tsUserRoleList.add(tsUserRole);
+                } catch (Exception e) {
+                    log.error(JSONArray.toJSONString(row));
+                    throw new RuntimeException("转换excel失败");
+                }
             });
             if (!saveBatch(tsUserList)) {
+                log.error(JSONObject.toJSONString(tsUserList));
                 throw new RuntimeException("导入学生信息失败");
             }
             if (!tsUserRoleService.saveBatch(tsUserRoleList)) {
+                log.error(JSONObject.toJSONString(tsUserRoleList));
                 throw new RuntimeException("导入初始角色分配失败");
             }
             return true;
